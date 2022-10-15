3c512a71073c25728ff417a737b82a4725505e23
HAKKOD <111317578+HAKKOD@users.noreply.github.com>
2022-10-15 09:22:14 +0000

    Add files via upload

--- belgeler/js/cihaz-kontrolu.js
+++ belgeler/js/cihaz-kontrolu.js
@@ -1,10 +1,16 @@
 function cihazKontrolu() {
     if (window.screen.width < 992) {
         if (window.location.pathname.substring(0, 7) != "/mobil/") {
-            window.location.replace(window.location.pathname.replace("/pc/", "/mobil/"));
+            if (window.location.pathname.includes("/pc/"))
+                window.location.replace(window.location.pathname.replace("/pc/", "/mobil/"));
+            else
+                window.location.replace("/mobil/");
         }
     } else if (window.location.pathname.substring(0, 4) != "/pc/") {
-        window.location.replace(window.location.pathname.replace("/mobil/", "/pc/"));
+        if (window.location.pathname.includes("/mobil/"))
+            window.location.replace(window.location.pathname.replace("/mobil/", "/pc/"));
+        else
+            window.location.replace("/pc/");
     }
 }
 cihazKontrolu();
